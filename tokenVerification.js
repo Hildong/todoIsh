e8677b1cc7034fd3e915b7d50a5eb7a688e9adb2
Hildong <philip.hilding@hotmail.com>
2020-07-15 11:20:36 +0200

    So my cookie isn't being read/ registered? :o

--- tokenVerification.js
+++ tokenVerification.js
@@ -9,12 +9,12 @@ app.use(cookieParser())
 
 module.exports = function (req, res, next) {
     let token = undefined
-    const authHeader = req.headers["cookie"].split(" ")[1]
+    const authHeader = req.headers["cookie"]//.split(" ")[1]
     console.log(authHeader)
     if(authHeader !== undefined) {
         token = authHeader.substring(6)
         console.log(token)
-    }else if(token === null || token === undefined) return res.status(403).send(`${req.headers["cookie"]}`);
+    }else if(token === null || token === undefined) return res.status(403).redirect(`https://todoappbyphiliphilding.herokuapp.com/`);
 
     jwt.verify(token, process.env.SECRET_TOKEN, (err, user) => {
         if(err) {
